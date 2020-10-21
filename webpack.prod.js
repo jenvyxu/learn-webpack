ccc657d52428225a60097696edc2f91b647cfded
xzw <1272117264@qq.com>
2020-10-21 12:13:54 +0800

    13. 使用px2rem-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -35,7 +35,14 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           'css-loader',
           'less-loader',
-          'postcss-loader'
+          'postcss-loader',
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              remPrecision: 8
+            }
+          }
         ]
       },
       {
