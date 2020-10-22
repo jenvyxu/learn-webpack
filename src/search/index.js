c00b70781cf1179517716579bb3133e3690000c4
xzw <1272117264@qq.com>
2020-10-22 13:46:57 +0800

    20. tree-shaking摇树优化

--- src/search/index.js
+++ src/search/index.js
@@ -5,11 +5,17 @@ import ReactDOM from 'react-dom'
 import logo from './image/logo.png'
 import './search.less'
 import '../../common/common'
+import { a, b } from './tree-shaking'
+
+if(false) {
+  a()
+}
 
 class Search extends React.Component {
   render() {
+    const text = b()
     return (<div className="search-text">
-      搜索文字的内容<img src={logo} />
+      搜索文字的内容{text}<img src={logo} />
     </div>)
   }
 }
